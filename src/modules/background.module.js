3587b84c7e2a74b07803d3a5e34d25a54338ab9b
__BEAST__ <mihailkalacev9@gmail.com>
2023-01-21 12:09:35 +0300

    last changes

--- src/modules/background.module.js
+++ src/modules/background.module.js
@@ -2,5 +2,17 @@ import {Module} from '../core/module'
 console.log('hello world')
 
 export class BackgroundModule extends Module {
-    
+    #randomColor = ['red','blue','green','skyblue','chartreuse','grey','brown']
+    constructor(type, text) {
+        super(type,text)
+
+    }
+    random(arr) {
+        const rand = Math.floor(Math.random()*arr.length);
+        const rValue = colorArray[rand];
+        return rValue
+    }
+    trigger() {
+       
+    }
 }
