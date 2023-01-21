ae58c9fce913032ea549e897fe2bf8bdb5da8685
IvanPazhgin <ivan.pazhgin@gmail.com>
2023-01-21 14:52:56 +0300

    init module random_sound

--- src/modules/random_sound.module.js
+++ src/modules/random_sound.module.js
@@ -1,8 +1,13 @@
 import {Module} from '../core/module'
+// Аналитика кликов (за установленное вами время) или Случайный звук
 
 export class Random_soundModule extends Module {
-  constructor() {
-    super()
+  // constructor() {
+  //   super()
+  // }
+
+  trigger() {
+    // super.trigger();
   }
 
   start() {
