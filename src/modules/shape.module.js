a25074b370cd3eb20ee46870000f9291ff34c84d
Maxim Rogozin <maximrogozin@icloud.com>
2023-01-21 15:10:04 +0500

    add random color in utils

--- src/modules/shape.module.js
+++ src/modules/shape.module.js
@@ -1,42 +1,30 @@
-import {Module} from '../core/module'
+import { Module } from '../core/module'
+import { generateRandomColor } from '../core/utils'
+import { random } from '..core/utils'
 
 export class ShapeModule extends Module {
     constructor (type, text){
-        super(text)
+        super(type)
+        this.text = text
     }
-    // функция для создания рандомного цвета
-    #generateRandomColor() {
-        const hexCodes = '0123456789ABCDEF'
-        let color = ''
-        for (let i = 0; i < 6; i++ ) {
-            color += hexCodes[Math.floor(Math.random() * hexCodes.length)]
-        }
-        return '#' + color
-    }
-
-    // функция для создания рандомной фигуры
-
+   
     createRandomShape(){
         const body = document.querySelector('body')
         const shape = document.createElement('div')
-        const size = this.#getRandomNumber(10, 100)
+        const size = random(10, 100)
         const {width, height} = body.getBoundingClientRect()
-        const x = this.#getRandomNumber(10, width - size)
-        const y = this.#getRandomNumber(10, height - size)
+        const x = random(10, width - size)
+        const y = random(10, height - size)
 
         shape.classList.add('circle')
         shape.style.position = 'absolute'
-        shape.style.width = `${this.#getRandomNumber(10, 100)}px`
-        shape.style.height = `${this.#getRandomNumber(10, 100)}px`
+        shape.style.width = `${random(10, 100)}px`
+        shape.style.height = `${random(10, 100)}px`
         shape.style.top = `${y}px`
         shape.style.left = `${x}px`
-        shape.style.borderRadius = `${this.#getRandomNumber(0, 60)}%`
-        shape.style.backgroundColor = this.#generateRandomColor()
+        shape.style.borderRadius = `${random(0, 60)}%`
+        shape.style.backgroundColor = generateRandomColor()
 
         body.append(shape)
-    }
-
-    #getRandomNumber(min, max) {
-        return Math.round(Math.random() * (max - min) + min)
     }
 }
