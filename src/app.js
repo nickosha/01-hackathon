7b45acd23216ed3e6f0a832a5c17bcea83f33d33
Виталий Николаенко <vitaliy@iMac-Vitalij.local>
2023-01-21 11:43:49 +0300

    add context menu

--- src/app.js
+++ src/app.js
@@ -1,2 +1,14 @@
 import './styles.css'
 
+import { ContextMenu } from './menu'
+
+let menu = new ContextMenu('#menu')
+menu.add()
+
+document.addEventListener('contextmenu', (e) => {
+    e.preventDefault();
+    console.log(menu);
+    console.log(menu.el)
+    menu.open()
+
+})
