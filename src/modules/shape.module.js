54d6cf516b3a63d64d3d8a833cd663eb13721ba5
Maxim Rogozin <maximrogozin@icloud.com>
2023-01-21 16:09:05 +0500

    optimization

--- src/modules/shape.module.js
+++ src/modules/shape.module.js
@@ -3,19 +3,16 @@ import { generateRandomColor } from '../core/utils'
 import { random } from '..core/utils'
 
 export class ShapeModule extends Module {
-    constructor (type, text){
+    constructor (type,){
         super(type)
-        this.text = text
     }
-   
-    createRandomShape(){
+    trigger() {
         const body = document.querySelector('body')
         const shape = document.createElement('div')
         const size = random(10, 100)
         const {width, height} = body.getBoundingClientRect()
         const x = random(10, width - size)
         const y = random(10, height - size)
-
         shape.classList.add('circle')
         shape.style.position = 'absolute'
         shape.style.width = `${random(10, 100)}px`
@@ -24,7 +21,17 @@ export class ShapeModule extends Module {
         shape.style.left = `${x}px`
         shape.style.borderRadius = `${random(0, 60)}%`
         shape.style.backgroundColor = generateRandomColor()
-
         body.append(shape)
+        setTimeout(shape.remove(), 1000)
     }
+
+    toHTML() {
+        const createShape = document.createElement('li')
+        createShape.classList = 'menu-item'
+        createShape.dataset = `${this.type}`
+        createShape.textContent = 'Создать фигуру'
+
+        return createShape
+      }
+  
 }
