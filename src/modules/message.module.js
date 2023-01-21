094fba2f5ca44472c84d61133888fd090f01cdd4
Valeriy Lapshov <vklapshov@gmail.com>
2023-01-21 12:27:30 +0300

    Added logic to module

--- src/modules/message.module.js
+++ src/modules/message.module.js
@@ -1,5 +1,11 @@
 import { Module } from '../core/module'
 
 export class MessageModule extends Module {
-
+    trigger() {
+        const quotes = ['000', '001', '010', '011', '100', '101', '110', '111']
+        const message = document.createElement('p')
+        message.textContent = quotes[random(0, quotes.length - 1)]
+        document.body.append(message)
+        setTimeout(() => { text.remove() }, 5000)
+    }
 }
